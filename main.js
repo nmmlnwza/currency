dc2888a56650d6569f48656558266db1de29a04a
nmmlnwza <jbq1551@gmail.com>
2024-06-13 09:12:23 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function del1() {
+    let cash = document.getElementById("lnwza");
+    let result = document.getElementById("result");
+    let sum = parseFloat(cash.value) * 37.50 ;
+    result.innerHTML = sum;
+}
+
+
+ 
